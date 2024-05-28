675d371a820e396075a2b48d1e2c3eaf05e798a3
Plamen Milanov <plamenpk@gmail.com>
2024-05-28 08:14:43 +0200

    Add register user

---
+++ authapp/app/api/userExists/route.js
@@ -0,0 +1,19 @@
+import { connectMongoDB } from "@/lib/mongodb";
+import User from "@/models/user";
+import { NextResponse } from "next/server";
+
+export async function POST(req) {
+  try {
+    await connectMongoDB();
+    const { email } = await req.json();
+    const user = await User.findOne({ email }).select("_id");
+    console.log("userExist: ", user);
+    return NextResponse.json({ user });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { message: "An error occurred while checking user existence." },
+      { status: 500 }
+    );
+  }
+}
---
+++ authapp/app/page.jsx
@@ -0,0 +1,11 @@
+import LoginForm from "@/components/LoginPage";
+
+const Home = () => {
+  return (
+    <main>
+      <LoginForm />
+    </main>
+  );
+}
+
+export default Home;
