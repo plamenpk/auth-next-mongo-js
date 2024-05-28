e2aa8444989dc9e98ad9959418edb4b7ee697d0f
Plamen Milanov <plamenpk@gmail.com>
2024-05-28 10:53:41 +0200

    Add logIn

--- authapp/app/page.jsx
+++ authapp/app/page.jsx
@@ -1,6 +1,13 @@
 import LoginForm from "@/components/LoginPage";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { authOptions } from "./api/auth/[...nextauth]/route";
 
-const Home = () => {
+const Home = async () => {
+  const session = await getServerSession(authOptions);
+
+  if (session) redirect("/dashboard");
+
   return (
     <main>
       <LoginForm />
--- authapp/components/LoginPage.jsx
+++ authapp/components/LoginPage.jsx
@@ -1,51 +1,62 @@
-// "use client";
+"use client";
 
 import Link from "next/link";
-// import { useState } from "react";
-// import { signIn } from "next-auth/react";
-// import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
-const LoginForm=() =>{
-  // const [email, setEmail] = useState("");
-  // const [password, setPassword] = useState("");
-  // const [error, setError] = useState("");
+const LoginForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
-  // const router = useRouter();
+  const router = useRouter();
 
-  // const handleSubmit = async (e) => {
-  //   e.preventDefault();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-  //   try {
+    try {
+      const res = await signIn("credentials", {
+        email,
+        password,
+        redirect: false,
+      });
 
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
+      if (res.error) {
+        setError("Invalid Credentials");
+        return;
+      }
+
+      router.replace("dashboard");
+    } catch (error) {
+      console.log('singIn', error);
+    }
+  };
 
   return (
     <div className="grid place-items-center h-screen">
       <div className="shadow-lg p-5 rounded-lg border-t-4 border-green-400">
         <h1 className="text-xl font-bold my-4">Login</h1>
 
-        <form  className="flex flex-col gap-3">
+        <form onSubmit={handleSubmit} className="flex flex-col gap-3">
           <input
-            //onChange={(e) => setEmail(e.target.value)}
+            onChange={(e) => setEmail(e.target.value)}
             type="text"
             placeholder="Email"
           />
           <input
-            //onChange={(e) => setPassword(e.target.value)}
+            onChange={(e) => setPassword(e.target.value)}
             type="password"
             placeholder="Password"
           />
           <button className="bg-green-600 text-white font-bold cursor-pointer px-6 py-2">
             Login
           </button>
-          {/* {error && (
+          {error && (
             <div className="bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2">
               {error}
             </div>
-          )} */}
+          )}
 
           <Link className="text-sm mt-3 text-right" href={"/register"}>
             Don`&apos`t have an account? <span className="underline">Register</span>
